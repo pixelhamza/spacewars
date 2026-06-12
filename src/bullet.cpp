63c93dc865b4836990ade8c4f075a6fc7a78a465
pixelhamza <nashithwk25@gmail.com>
2026-06-12 17:41:52 +0530

    add bullet object

---
+++ src/bullet.cpp
@@ -0,0 +1,12 @@
+#include "bullet.hpp"
+
+Bullet::Bullet(Vector2 position,float speed){
+    this->speed = speed; 
+    this->position = position;
+}
+
+void Bullet::Draw(){
+    DrawRectangle(position.x - 2, position.y - 2, 8, 20, Fade(YELLOW, 0.3f));
+    DrawRectangleGradientV(position.x, position.y, 4, 16, WHITE, YELLOW);
+}
+
---
+++ src/bullet.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "raylib.h"
+
+class Bullet{ 
+    public: 
+        Bullet(Vector2 position,float speed); 
+        void Draw();
+        void Update();
+
+    private: 
+        Vector2 position; 
+        float speed;
+};
