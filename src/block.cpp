d9811dae208bed2d28e073fb1a9edeb9e9470c53
pixelhamza <nashithwk25@gmail.com>
2026-06-16 18:22:36 +0530

    create block object for obstacle

---
+++ src/block.cpp
@@ -0,0 +1,5 @@
+#include "block.hpp"
+
+Block::Block(Vector2 position){ 
+    this->positio = position;
+}
---
+++ src/block.hpp
@@ -0,0 +1,11 @@
+#include "raylib.h"
+
+class Block{ 
+    public: 
+        Block(Vector2 position);
+        void Draw();
+
+    private: 
+        Vector2 position;
+
+};
